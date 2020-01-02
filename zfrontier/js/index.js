ff0afa1eda93632292aa681a17349ead120de4e1
xiaofei <1178862608@qq.com>
2020-01-02 20:32:33 +0800

    左侧列表动作实现

--- zfrontier/js/index.js
+++ zfrontier/js/index.js
@@ -1,2 +1,14 @@
 // 动作: 左侧的列表鼠标划过显示
-var 
+// 1. 查找触发事件的元素  div里的第一个a
+var eledivs=document.querySelectorAll("div.leftList>div");
+// 2. 绑定事件处理函数
+for(var elediv of eledivs){
+    elediv.onmouseover=function(){
+        console.log("1");
+        this.className="open";
+        // var openUl=document.querySelector("div.open>ul");
+    }
+    elediv.onmouseout=function(){
+        this.className="";
+    }
+}
