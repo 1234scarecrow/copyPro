c6d3556de54090a07cf5a6f4450dfee8f7dddb94
xiaofei <1178862608@qq.com>
2020-01-03 20:43:32 +0800

    使用iconfont生成图标，并通过DOM添加动作

--- zfrontier/js/index.js
+++ zfrontier/js/index.js
@@ -1,14 +1,22 @@
 // 动作: 左侧的列表鼠标划过显示
-// 1. 查找触发事件的元素  div里的第一个a
+// 1. 查找触发事件的元素  div
 var eledivs=document.querySelectorAll("div.leftList>div");
 // 2. 绑定事件处理函数
 for(var elediv of eledivs){
+    // 查找要修改的元素  span
+    // var spans=document.querySelectorAll("div.leftList span");
     elediv.onmouseover=function(){
         console.log("1");
         this.className="open";
         // var openUl=document.querySelector("div.open>ul");
+        this.children[1].className="iconfont icon-down"
+        
+        // span.className="";
     }
     elediv.onmouseout=function(){
         this.className="";
+        // span.className="iconfont icon-next";
+        this.children[1].className="iconfont icon-next";
+
     }
 }
